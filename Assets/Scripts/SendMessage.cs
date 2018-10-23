d51c71aa68597da7ba1cad976fe46607028177c3
Mike <mgrin10@student.dkit.ie>
2018-10-23 12:17:04 +0100

    Initial Commit

---
+++ Assets/Scripts/SendMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessage : MonoBehaviour {
+
+	public void SendFungusMessage(string s)
+    {
+        Fungus.Flowchart.BroadcastFungusMessage(s);
+    }
+}
